9fd4d8448503138491fa618d7658f0b61f91aeda
p-thak <cpathakis@gmail.com>
2017-02-04 17:11:37 -0700

    added methods to the facade and poller classes

---
+++ app/src/main/java/com/example/alec/phase_05/Client/Poller.java
@@ -0,0 +1,8 @@
+package com.example.alec.phase_05.Client;
+
+/**
+ * Created by clarkpathakis on 2/4/17.
+ */
+
+public class Poller {
+}
---
+++ app/src/main/java/com/example/alec/phase_05/Shared/Player.java
@@ -0,0 +1,8 @@
+package com.example.alec.phase_05.Shared;
+
+/**
+ * Created by clarkpathakis on 2/4/17.
+ */
+
+public class Player {
+}
