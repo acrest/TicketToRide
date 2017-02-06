d641f651a474dcb45baf4f773f545943054f0462
acrest <aleccrestani@gmail.com>
2017-02-06 11:42:37 -0700

    added player, game, serverCommunicator, serverFacade, serverModel

--- app/src/main/java/com/example/alec/phase_05/Server/ServerCommunicator.java
+++ app/src/main/java/com/example/alec/phase_05/Server/ServerCommunicator.java
@@ -1,7 +1,46 @@
 package com.example.alec.phase_05.Server;
 
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
 /**
  * Created by Alec on 1/20/17.
  */
 public class ServerCommunicator {
+
+    private static final int MAX_WAITING_CONNECTIONS = 12;
+
+    private HttpServer server;
+
+    private void run(String portNumber) {
+        System.out.println("Initializing HTTP Server");
+        try {
+            server = HttpServer.create(
+                    new InetSocketAddress(Integer.parseInt(portNumber)),
+                    MAX_WAITING_CONNECTIONS);
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        server.setExecutor(null); // use the default executor
+
+        System.out.println("Creating contexts");
+        server.createContext("/login", new TrimHandler());
+        server.createContext("/createGame", new ParseIntergerHandler());
+        server.createContext("/createPlayer", new ToLowerCaseHandler());
+        server.createContext("/exec", new ExecCommandHandler());
+
+        System.out.println("Starting server");
+        server.start();
+    }
+
+    public static void main(String[] args) {
+        String portNumber = "8080";
+        new ServerCommunicator().run(portNumber);
+    }
+
 }
---
+++ app/src/main/java/com/example/alec/phase_05/Shared/Player.java
@@ -0,0 +1,12 @@
+package com.example.alec.phase_05.Shared;
+
+/**
+ * Created by Alec on 2/5/17.
+ */
+public class Player {
+
+    public String name;
+    public Integer ID;
+    public String password;
+
+}
