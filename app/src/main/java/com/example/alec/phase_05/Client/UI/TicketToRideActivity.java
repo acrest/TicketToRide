93fac9ed100fb4e497667b2a2b2411a44a06a20c
p-thak <cpathakis@gmail.com>
2017-02-23 20:20:47 -0700

    slider

--- app/src/main/java/com/example/alec/phase_05/Client/UI/TicketToRideActivity.java
+++ app/src/main/java/com/example/alec/phase_05/Client/UI/TicketToRideActivity.java
@@ -3,16 +3,40 @@ package com.example.alec.phase_05.Client.UI;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import com.example.alec.phase_05.R;
 
 public class TicketToRideActivity extends Activity {
+    private ListView mDrawerList;
+    private ArrayAdapter<String> mAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_ticket_to_ride);
+        mDrawerList = (ListView)findViewById(R.id.navList);
+
     }
+
+    private void addDrawerItems() {
+        String[] osArray = { "I", "am", "a", "test", "wizard" };
+        mAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, osArray);
+        mDrawerList.setAdapter(mAdapter);
+        mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(TicketToRideActivity.this, "Time for an upgrade!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+
 }
