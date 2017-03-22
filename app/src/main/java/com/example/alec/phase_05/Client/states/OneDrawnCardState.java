38dbce788882fc1d44e6ad4d4f6d8e71fe299c45
p-thak <cpathakis@gmail.com>
2017-03-22 12:25:57 -0600

    Rainbow state functionality added.

--- app/src/main/java/com/example/alec/phase_05/Client/states/OneDrawnCardState.java
+++ app/src/main/java/com/example/alec/phase_05/Client/states/OneDrawnCardState.java
@@ -6,6 +6,8 @@ import com.example.alec.phase_05.Shared.model.DestinationCard;
 import com.example.alec.phase_05.Shared.model.Game;
 import com.example.alec.phase_05.Shared.model.GameState;
 import com.example.alec.phase_05.Shared.model.StateWarning;
+import com.example.alec.phase_05.Shared.model.TrainCard;
+import com.example.alec.phase_05.Shared.model.TrainType;
 
 /**
  * Created by clarkpathakis on 3/22/17.
@@ -35,8 +37,12 @@ public class OneDrawnCardState implements GameState {
         System.out.println("Train card picked from face up cards.");
         // check not rainbow card.
         facade.pickTrainCard(cardIndex);
-        state.setTurnState(state.getOneDrawnOnePickedCardState());
-
+        TrainCard pickedCard = game.getVisibleCard(cardIndex);
+        if (pickedCard.getType().equals(TrainType.LOCOMOTIVE)) {
+            state.setTurnState(state.getRainbowCardState());
+        } else {
+            state.setTurnState(state.getOneDrawnOnePickedCardState());
+        }
     }
 
     @Override
