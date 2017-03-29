b51b4344b625951b27bd0ae2f2888f1d4b45947c
Molly <mollybarrott@gmail.com>
2017-03-28 19:52:15 -0600

    text size change

--- app/src/main/java/com/example/alec/phase_05/Client/UI/StartScreenActivity.java
+++ app/src/main/java/com/example/alec/phase_05/Client/UI/StartScreenActivity.java
@@ -100,16 +100,14 @@ public class StartScreenActivity extends AppCompatActivity {
                 }
             }
         });
-
-
-
+        
     }
 
     private void setIpAndTrains(){
-        firstIpAddress.setText("192");
-        secondIpAddress.setText("168");
-        thirdIpAddress.setText("122");
-        fourthIpAddress.setText("1");
+        firstIpAddress.setText("10");
+        secondIpAddress.setText("24");
+        thirdIpAddress.setText("67");
+        fourthIpAddress.setText("45");
     }
 
     private void setTrainCount(){
